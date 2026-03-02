00095a1784a7cca057de4872fd6ec4fbc54499b0
Pseudo Coder <avijain1607@gmail.com>
2026-03-02 19:00:19 +0530

    Better Versiont

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST || 'localhost',
     user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || '',
+    password: process.env.MYSQL_PASSWORD || 'pseudo@coder',
     database: process.env.MYSQL_DATABASE || 'campusprint',
     waitForConnections: true,
     connectionLimit: 10,
